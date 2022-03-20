c29fe6ea524031cd61d06374ecf3c918675c18d6
eros <eros.ru@outlook.com>
2022-03-20 09:41:02 +0900

    fd

---
+++ Calcs/__init__.py
@@ -0,0 +1,12 @@
+from .calc_algo_v5 import (
+    config, 
+    parsed_data, 
+    calc_results
+)
+
+from .calc_algo_v5 import (
+    process_master_file, 
+    process_case_file, 
+    do_calculation,
+    output_result_to_excel
+)
